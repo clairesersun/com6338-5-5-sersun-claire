50d617b06949d8220bf6de9af10e713305df312f
Claire Sersun <claire.sersun@gmail.com>
2022-06-10 11:54:18 -0600

    add //

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var form = document.querySelector('form')
 var newToDos = document.querySelector("li button")
 var doneToDos = document.querySelector(".done")
+var input = document.querySelector('input')
 
 //how? by pressing the enter key || clicking the add button
 form.onsubmit = function(e) {
@@ -12,21 +13,23 @@ form.onsubmit = function(e) {
     // var button = document.createElement("button")
     // node.appendChild(button)
     // document.getElementById("todo-list").appendChild(node)
-    // this.toDo.value = ""
     //add text that user inputs as a todo
     var li = document.createElement("li") 
     var button = document.createElement("button")
     button.innerHTML = this.toDo.value
+    this.toDo.value = ""
     //text withing input should appear below the form inside of the ul#todo-list element.
     document.getElementById("todo-list").appendChild(li)
     //create a new button element nested within a li element
     document.querySelector(li).appendChild(button)
     if (button) {
         button.addEventListener('click', function() {
+//if it is, The item should be marked done by striking through the words using the text-decoration CSS property
             newToDos.style.textDecoration = "line-through"
             newToDos.classList.add("done")
         })}
     if (doneToDos){
+//also watch to see if an item that has been marked as done is clicked, if so clicking an item that's been marked as done should remove the item from the list
         li.remove()
     }
 }
