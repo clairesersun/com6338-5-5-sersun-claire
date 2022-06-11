94a9a4d33a3154dfdf8e790f2bb61b5ec2c64c70
Claire Sersun <claire.sersun@gmail.com>
2022-06-11 16:02:16 -0600

    add and cross out to do items

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var form = document.querySelector('form')
-var newToDos = document.querySelector("li button")
 var doneToDos = document.querySelector(".done")
-var input = document.querySelector('input')
+var input = document.querySelector('#users-toDo')
 
 //how? by pressing the enter key || clicking the add button
 form.onsubmit = function(e) {
-    debugger
     e.preventDefault()
-    if (this.toDo.value = "" || /^\s*$/) return
+    //So for the input validation, we can create the variable that will store the information from the input field and make sure we trim the white spaces from it. We can also say that if the input value is false (empty), then we just return and not run any code. You also want to make sure that once this is done, the input field on the form is reset to empty.
+    var userInput = input.value
+    var inputArr = userInput.trim()
+    if (!inputArr) {
+        userInput = ""
+        return
+    }
     // var node = document.createElement("li")
     // var buttonText = document.getElementById("users-toDo").value
     // var button = document.createElement("button")
@@ -16,17 +20,19 @@ form.onsubmit = function(e) {
     //add text that user inputs as a todo
     var li = document.createElement("li") 
     var button = document.createElement("button")
-    button.innerHTML = this.toDo.value
+    document.getElementById("todo-list").appendChild(li)
+    li.appendChild(button)
+    button.textContent = userInput
+    //In reference to the button and list item, you have the right variables created for var li and var button. I wouldn’t do button.innerHTML = this.toDo.value but instead would append a child (in this case button) to the li variable and then change the text content of the button to the variable you created above that stores the user’s information.
     this.toDo.value = ""
     //text withing input should appear below the form inside of the ul#todo-list element.
-    document.getElementById("todo-list").appendChild(li)
     //create a new button element nested within a li element
-    document.querySelector(li).appendChild(button)
+    //document.querySelector("li").appendChild(button)
     if (button) {
         button.addEventListener('click', function() {
 //if it is, The item should be marked done by striking through the words using the text-decoration CSS property
-            newToDos.style.textDecoration = "line-through"
-            newToDos.classList.add("done")
+            button.style.textDecoration = "line-through"
+            button.classList.add("done")
         })}
     if (doneToDos){
 //also watch to see if an item that has been marked as done is clicked, if so clicking an item that's been marked as done should remove the item from the list
